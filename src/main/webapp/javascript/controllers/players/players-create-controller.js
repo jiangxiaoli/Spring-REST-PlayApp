567c5a3812f22e5d8f55f270ede9e0d4f0bae46b
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-04-12 16:43:16 -0700

    transfer $http post data format to requestParams

--- src/main/webapp/javascript/controllers/players/players-create-controller.js
+++ src/main/webapp/javascript/controllers/players/players-create-controller.js
@@ -3,23 +3,42 @@
 angular.module("snippetShare")
     .controller("PlayersCreateController", function ($http) {
         console.log("player create ctrl");
-
         var controller = this;
-
-        controller.player = null;
-
         this.savePlayer = function (player) {
             //handel errors
-            controller.errors = null;
-            $http({ method:"POST", url: "/players", data: player })
-                .success(function (data) {
+            //controller.errors = null;
+            //$http({ method:"POST", url: "/players", data: player })
+            //    .success(function (data) {
+            //        console.log("in success");
+            //        console.log(data);
+            //    })
+            //    .catch(function (player) {
+            //        console.log("in error");
+            //        console.log(player);
+            //        controller.errors.player.error;
+            //    });
+
+
+            //transfer $http send data format to request param
+            var transform = function(player){
+                return $.param(player);
+            };
+
+            $http.post("/players", player, {
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+                transformRequest: transform
+            })
+                .success(function(data, status, headers, config) {
                     console.log("in success");
                     console.log(data);
+                    console.log(status);
                 })
-                .catch(function (player) {
+                .error(function(data, status, headers, config) {
                     console.log("in error");
-                    console.log(player);
-                    controller.errors.player.error;
+                    console.log(data);
+                    console.log(status);
                 });
+
+
         }
     });
